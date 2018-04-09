402f478a8c267551a079415151113f1a26f8d844
Fábio Dias Soares <fabio.soares@blendmobi.com>
2018-04-09 14:13:54 -0300

    urlendcoded

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.urlencoded({"extended": true}));
 app.use(bodyParser.json({}));
 
 app.post('/leave', (req, res) => {
