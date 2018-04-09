5a926326e50ff308123d1f7d26afe125202373d3
Fábio Dias Soares <fabio.soares@blendmobi.com>
2018-04-09 14:04:39 -0300

    logd body and response

--- index.js
+++ index.js
@@ -5,33 +5,41 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json({}));
-app.post('/leave', (req, res) => {
 
+app.post('/leave', (req, res) => {
+  console.log('/leave', req.body);
   let hasVacation = (+req.body.userID === 3);
   let period = hasVacation ? '04/06/18,22/06/18' : null;
   let negativeResponse = hasVacation ? false : ['Você não tem férias agendadas, você poderá realizar a solicitação através do portal do RH.'];
-  res.status(200).send({hasVacation, period, negativeResponse});
+  let response = {hasVacation, period, negativeResponse};
+  console.log('/leave', response);
+  res.status(200).send(response);
 });
 
 app.post('/paycheck', (req, res) => {
+  console.log('/paycheck', req.body);
   let paycheck = {
     '2': 'ccristina.pdf',
     '3': 'asantos.pdf'
   };
 
   let myPaycheck = paycheck[req.body.userID] || false;
-
-  return res.status(200).json({
+  let response = {
     'hasPayment': (myPaycheck ? true : null),
     'file': myPaycheck ? (`http://mobi.blendit.com.br:3300/downloads/${myPaycheck}`) : null,
     'myPaycheckMonth': '03/2018',
     'paidDate': '25/03/2018',
     'negativeResponse': myPaycheck ? false : 'Sinto muito, você ainda não tem contracheques disponíveis.'
-  });
+  };
+  console.log('/paycheck', response);
+  return res.status(200).json(response);
 });
 
 app.post('/hours', (req, res) => {
-  return res.status(200).json({'hasHours': true, 'hourBank': (+req.body.userID === 3 ? '03:10' : '25:12')});
+  console.log('/hours', req.body);
+  let response = {'hasHours': true, 'hourBank': (+req.body.userID === 3 ? '03:10' : '25:12')};
+  console.log('/hours', response);
+  return res.status(200).json(response);
 });
 
 app.listen(8809, () => {
